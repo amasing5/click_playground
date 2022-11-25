ac1d8f1761c7d4390d6bd18f25a538dee1325020
Amandeep Singh <amandeep.singh2@meraki.net>
2022-11-25 15:26:57 +0530

    Rename ClickRouter to ClickPlayground

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
-CLICK_RUNNER=click_playground/click_router
+CLICK_RUNNER=click_playground/click_pg
 CLICK_ENDPOINT=click_playground/click_ep
 IMAGE_TAG=$CLICK_BUILDER
 
@@ -9,9 +9,9 @@ build() {
 	IMAGE_TYPE=$1
 
 	case "$IMAGE_TYPE" in
-	"click_playground/click_router")
+	"click_playground/click_pg")
 		docker build --tag "$CLICK_RUNNER" \
-			--file clickRouter.Dockerfile .
+			--file clickPlayground.Dockerfile .
 		;;
 	"click_playground/click_ep")
 		docker build  --tag "$CLICK_ENDPOINT" \
@@ -23,5 +23,5 @@ build() {
      esac
 }
 
-build click_playground/click_router
+build click_playground/click_pg
 build click_playground/click_ep
